a923499b5228f41bca40c9687b98e5ba2d029837
saki <stanislaw.darowski101@gmail.com>
2024-11-22 03:42:52 +0100

    done request

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,57 @@
 use std::io::{BufRead, Read, Write};
+use std::net::Shutdown;
 
 mod my_http;
 
+pub struct HTTPHeader {
+	name: String,
+	value: String,
+}
+
 pub struct Request {
 	pub part_counter: u8,
-	request_line_byte_buffer: Vec<u8>,
-	headers_byte_buffer: Vec<u8>,
-	body_byte_buffer: Vec<u8>,
+	is_complete: bool,
+	internal_buffer: Vec<u8>,
 
-	is_new_line: bool,
+	new_line_hold: bool,
+
 	method: String,
 	path: String,
 	query: String,
-	headers: Vec<String>,
+	http_version: String,
+	headers: Vec<HTTPHeader>,
 	body: String,
-	version: String,
+	content_length: usize,
 }
 
 impl Request {
 	pub fn new() -> Self {
 		Request {
 			part_counter: 0,
-			request_line_byte_buffer: Vec::new(),
-			headers_byte_buffer: Vec::new(),
-			body_byte_buffer: Vec::new(),
-			is_new_line: false,
+			is_complete: false,
+			internal_buffer: Vec::with_capacity(0x400),
+
+			new_line_hold: false,
 
 			method: String::new(),
 			path: String::new(),
 			query: String::new(),
-			version: String::new(),
+			http_version: String::new(),
 			headers: Vec::new(),
+
+			content_length: 0,
 			body: String::new(),
 		}
 	}
 	pub fn from_str(text: &str) -> Self {
 		let mut s = Self::new();
 		s.push_bytes(text.as_bytes());
-		s.parse();
 		s
 	}
 
 	fn parse_request_first_line(&mut self) -> Result<(), &'static str> {
 		let line_str = String::from_utf8_lossy(
-			self.request_line_byte_buffer.as_slice())
+			self.internal_buffer.as_slice())
 			.to_string();
 		let line_trim = line_str.trim();
 
@@ -72,7 +80,7 @@ impl Request {
 							};
 						}
 
-						2 => self.version = buffer.iter().collect(),
+						2 => self.http_version = buffer.iter().collect(),
 						_ => return Err("bad format")
 					}
 					buffer.clear();
@@ -87,81 +95,87 @@ impl Request {
 		Ok(())
 	}
 
-	fn parse_request_headers(&mut self) -> Result<(), &'static str> {
-		let mut header_buffer = Vec::<u8>::with_capacity(0x400);
-		let mut cr_hold = false;
-		for (i, c) in self.headers_byte_buffer.iter().enumerate() {
-			if *c == b'\r' {
+	pub fn push_bytes(&mut self, buffer: &[u8]) {
+		for c in buffer.iter().cloned() {
+			if self.is_complete {
+				return;
+			}
+
+			if c == b'\r' {
 				continue;
 			}
-			if *c == b'\n' {
-				if !cr_hold {
-					cr_hold = true;
-					self.headers.push(
-						String::from_utf8_lossy(header_buffer.as_slice()).to_string());
-					header_buffer.clear();
-				}
-			} else {
-				header_buffer.push(*c);
-				cr_hold = false;
-			}
-		}
-		Ok(())
-	}
 
-	pub fn push_bytes(&mut self, buffer: &[u8]) {
-		for c in buffer {
-			match self.part_counter {
-				0 => self.request_line_byte_buffer.push(*c),
-				1 => self.headers_byte_buffer.push(*c),
-				2 => self.body_byte_buffer.push(*c),
-				_ => panic!("invalid part_counter ({})", self.part_counter)
+			if c != b'\n' {
+				self.internal_buffer.push(c);
 			}
 
-			if *c == b'\n' {
+			if c == b'\n' {
 				match self.part_counter {
 					0 => {
-						println!("push_bytes, \\n - parse first line");
 						self.parse_request_first_line()
 							.expect("bad first line");
 						self.part_counter = 1;
-						self.is_new_line = true;
+						self.internal_buffer.clear();
 					}
 					1 => {
-						if self.is_new_line {
-							self.part_counter = 2;
-							self.parse_request_headers().expect("bad headers");
+						if !self.new_line_hold {
+							let current_header_line = String::from_utf8_lossy(self.internal_buffer.as_slice())
+								.to_string();
+							let mut header_parts_it = current_header_line.split(':');
+							let mut header_name = header_parts_it.next()
+								.unwrap().trim().to_string();
+							let mut header_val = match (header_parts_it.next()) {
+								Some(v) => String::from(v).trim().to_string(),
+								None => String::new(),
+							};
+							if header_name == "content-length" {
+								self.content_length = match header_val.parse::<usize>() {
+									Ok(v) => v,
+									_ => 0
+								};
+							}
+
+							self.headers.push(HTTPHeader {
+								name: header_name,
+								value: header_val,
+							});
+							self.internal_buffer.clear();
 						} else {
-							self.is_new_line = true;
+							self.part_counter = 2;
 						}
 					}
 					_ => {}
 				}
-			} else if *c != b'\r' {
-				self.is_new_line = false;
+				self.new_line_hold = true;
+			} else {
+				self.new_line_hold = false;
+			}
+
+			if self.part_counter == 2
+				&& !self.is_complete
+				&& self.internal_buffer.len() >= self.content_length {
+				self.body = String::from_utf8_lossy(self.internal_buffer.as_slice()).to_string();
+				self.internal_buffer.clear();
+				self.is_complete = true;
 			}
 		}
 	}
+}
 
-	pub fn parse(&mut self) -> Result<(), &'static str> {
-		if self.part_counter != 2 {
-			return Err("incomplete request.");
+impl std::fmt::Display for Request {
+	fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+		writeln!(f, "HTTP request (version={}) /{}",
+				 self.http_version,
+				 if self.is_complete { "complete" } else { "incomplete" })?;
+		writeln!(f, "method={}", self.method)?;
+		writeln!(f, "path={}", self.path)?;
+		writeln!(f, "query=\"{}\"", self.query)?;
+		writeln!(f, "== headers ==")?;
+		for h in &self.headers {
+			writeln!(f, "-> [{}]: [{}]", h.name, h.value)?;
 		}
-
-		println!("request");
-		println!("method={}", self.method);
-		println!("path={}", self.path);
-		println!("query={}", self.query);
-		println!("version={}", self.version);
-
-		println!("== headers ({}) ==", self.headers.len());
-		for h in self.headers.iter() {
-			println!("-> {}", h);
-		}
-
-		println!("== body ==");
-		println!("{}", self.body);
-
+		writeln!(f, "== body (length={}) ==", self.content_length)?;
+		writeln!(f, "{}", self.body)?;
 		Ok(())
 	}
 }
@@ -197,19 +211,25 @@ fn handle_connection(mut stream: std::net::TcpStream) {
 		println!("read {} bytes", n);
 
 		if n == 0 {
-			println!("connection closed");
+			println!("connection closed by peer");
 			break;
 		}
 
 		req.push_bytes(buffer[..n].as_ref());
 
-		if req.part_counter >= 2 {
+		if req.is_complete {
 			break;
 		}
 	}
 
-	req.parse().expect("failed to parse");
+	println!("{}", req);
 
-	stream.write("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).expect("failed to write");
+	println!("responding...");
+	stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).expect("failed to write");
+	stream.flush().expect("failed to flush");
+
+	println!("closing connection...");
+	stream.shutdown(Shutdown::Write).expect("failed to close connection");
+	stream.shutdown(Shutdown::Read).expect("failed to close connection");
 	println!("done");
 }
