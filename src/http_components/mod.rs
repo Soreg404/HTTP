f0cc5f5b9881942df34e4e81c7bfccc6e5133d69
saki <stanislaw.darowski101@gmail.com>
2025-06-13 23:05:26 +0200

    small refactor

--- src/http_components/mod.rs
+++ src/http_components/mod.rs
@@ -1,4 +1,4 @@
-pub mod mime_types;
+pub mod mime_type;
 pub mod parse_error;
 pub mod header;
 pub mod url;
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ mod http_components;
 
 pub use crate::http_components::attachment::*;
 pub use crate::http_components::header::*;
-pub use crate::http_components::mime_types::*;
+pub use crate::http_components::mime_type::*;
 pub use crate::http_components::partial_request::*;
 pub use crate::http_components::partial_request::*;
 pub use crate::http_components::request::*;
