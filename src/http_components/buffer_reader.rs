c0d846f35801f260d296d1f093fab0fda3a7e59d
saki <stanislaw.darowski101@gmail.com>
2025-06-25 17:10:31 +0200

    done buffer_reader, a helper module

--- src/http_components/buffer_reader.rs
+++ src/http_components/buffer_reader.rs
@@ -2,36 +2,31 @@
 enum ReadState {
 	#[default]
 	Ready,
-	SkipOne,
-	SkipSpaces,
-	ReadToken,
 	ReadLine,
-	ReadBlob(usize),
-	Finished,
+	ReadLineFinished,
+	ReadUntil(Vec<u8>),
+	ReadUntilFinished,
+	ReadExact(usize),
+	ReadExactFinished,
 }
-use ReadState::*;
 
-#[derive(PartialEq, Debug)]
-pub enum AdvanceResult {
-	Done,
-	Continue,
-}
-use AdvanceResult::*;
+use std::ops::Add;
+use ReadState::*;
 
 #[derive(Default)]
 pub struct BufferReader {
 	internal_buffer: Vec<u8>,
 	read_head: usize,
+
 	read_start: usize,
+	read_end: usize,
 
 	read_state: ReadState,
 }
 
-#[derive(Debug)]
-struct BufferIterationData {
-	current_byte: u8,
-	read_start: usize,
-	read_head: usize,
+enum LineEnding {
+	LF,
+	CRLF,
 }
 
 impl BufferReader {
@@ -39,176 +34,148 @@ impl BufferReader {
 		self.internal_buffer.extend_from_slice(data);
 	}
 
-	fn iterate_on_buffer<F>(&mut self, mut f: F) -> AdvanceResult
-	where
-		F: FnMut(BufferIterationData) -> AdvanceResult,
-	{
-		while self.read_head < self.internal_buffer.len() {
-			match f(
-				BufferIterationData {
-					current_byte: self.internal_buffer[self.read_head],
-					read_start: self.read_start,
-					read_head: self.read_head,
-				}
-			) {
-				Done => return Done,
-				Continue => {}
-			};
-			self.read_head += 1;
-		};
-		Continue
-	}
-
 	pub fn advance(&mut self) -> bool {
-		match self.read_state {
-			Ready | Finished => true,
-			SkipOne => {
-				if self.read_head + 1 < self.internal_buffer.len() {
+		match &self.read_state {
+			Ready
+			| ReadLineFinished
+			| ReadUntilFinished
+			| ReadExactFinished
+			=> true,
+
+			ReadLine => {
+				while self.read_head < self.internal_buffer.len() {
+					match self.check_crlf_at_head() {
+						None => {}
+						Some(ending) => {
+							match ending {
+								LineEnding::LF => {
+									self.read_end = self.read_head;
+									println!("got LF ending");
+								}
+								LineEnding::CRLF => {
+									self.read_end = self.read_head.checked_sub(1)
+										.expect("read_head should be > 0 here");
+									println!("got CRLF ending; read_start={}, read_end={}",
+											 self.read_start, self.read_end);
+								}
+							}
+							self.read_state = ReadLineFinished;
+							return true;
+						}
+					}
 					self.read_head += 1;
-					self.read_state = Ready;
-					true
-				} else {
-					false
 				}
+				false
 			}
-			ReadBlob(blob_size) => {
-				match self.iterate_on_buffer(|data| {
-					if (data.read_head + 1) - data.read_start == blob_size {
-						Done
-					} else {
-						Continue
-					}
-				}) {
-					Done => {
-						self.read_state = Finished;
-						true
-					}
-					Continue => false
-				}
-			}
-			_ => {
-				match self.read_state {
-					SkipSpaces => {
-						match self.iterate_on_buffer(|data| {
-							return if data.current_byte.is_ascii_whitespace() {
-								// todo: error checking - illegal whitespace bytes
-								Continue
-							} else {
-								Done
-							};
-						}) {
-							Done => {
-								self.read_state = Ready;
-								true
-							}
-							Continue => false,
-						}
-					}
-					ReadToken => {
-						match self.iterate_on_buffer(|data| {
-							if data.current_byte.is_ascii_whitespace() {
-								Done
-							} else {
-								Continue
-							}
-						}) {
-							Done => {
-								self.read_state = Finished;
-								true
-							}
-							Continue => false
-						}
-					}
-					ReadLine => {
-						match self.iterate_on_buffer(|data| {
-							if data.current_byte == b'\n' {
-								Done
-							} else {
-								Continue
-							}
-						}) {
-							Done => {
-								self.read_state = Finished;
-								true
-							}
-							Continue => false
-						}
-					}
 
-					Ready | Finished | ReadBlob(_) | SkipOne => unreachable!()
+			ReadUntil(sequence) => {
+				while self.read_head < self.internal_buffer.len() {
+					if self.internal_buffer[0..=self.read_head]
+						.ends_with(sequence.as_slice()) {
+						self.read_end = (self.read_head + 1).checked_sub(sequence.len())
+							.expect("invalid head position");
+						self.read_state = ReadUntilFinished;
+						return true;
+					}
+					self.read_head += 1;
 				}
+				false
+			}
+
+			ReadExact(size) => {
+				while self.read_head < self.internal_buffer.len() {
+					if (self.read_head + 1) - self.read_start == *size {
+						self.read_state = ReadExactFinished;
+						self.read_end = self.read_head + 1;
+						return true;
+					}
+					self.read_head += 1;
+				}
+				false
 			}
 		}
 	}
 
-	pub fn skip_spaces(&mut self) {
-		match self.read_state {
-			Ready | Finished => {
-				self.read_state = SkipSpaces;
-			}
-			_ => panic!("invalid state")
+	fn check_crlf_at_head(&self) -> Option<LineEnding> {
+		if self.internal_buffer.get(self.read_head) != Some(&b'\n') {
+			return None;
 		}
-	}
-
-	fn set_read_state(&mut self, state: ReadState) {
-		self.read_state = state;
-		assert!(self.read_head == 0 || self.read_start != self.read_head,
-				"invalid head state: attempted to read on the same position twice");
-		self.read_start = self.read_head;
-	}
-
-	pub fn read_token(&mut self) -> Option<&[u8]> {
-		match self.read_state {
-			ReadToken => None,
-			Ready => {
-				self.set_read_state(ReadToken);
-				None
+		match self.read_head.checked_sub(1) {
+			None => Some(LineEnding::LF),
+			Some(idx) => {
+				match self.internal_buffer.get(idx) {
+					None => unreachable!("invalid head / \
+										head not synced with buffer"),
+					Some(v) => {
+						if v == &b'\r' {
+							Some(LineEnding::CRLF)
+						} else {
+							Some(LineEnding::LF)
+						}
+					}
+				}
 			}
-			Finished => {
-				self.read_state = Ready;
-				self.internal_buffer
-					.get(self.read_start..self.read_head)
-			}
-			_ => panic!("invalid state")
-		}
-	}
-
-	pub fn read_blob(&mut self, blob_size: usize) -> Option<&[u8]> {
-		match self.read_state {
-			ReadBlob(_) => None,
-			Ready => {
-				self.set_read_state(ReadBlob(blob_size));
-				None
-			}
-			Finished => {
-				self.read_state = SkipOne;
-				self.internal_buffer
-					.get(self.read_start..=self.read_head)
-			}
-			_ => panic!("invalid state")
 		}
 	}
 
 	pub fn read_line(&mut self) -> Option<&[u8]> {
 		match self.read_state {
-			ReadLine => None,
 			Ready => {
-				println!("read_line: start_pos={}", self.read_start);
-				self.set_read_state(ReadLine);
+				self.read_start = self.read_head;
+				self.read_state = ReadLine;
 				None
 			}
-			Finished => {
-				self.read_state = SkipOne;
-				let line = &self.internal_buffer
-					[self.read_start..self.read_head];
-				println!("read_line finished: line={:?}, start={}, head={}", line, self.read_start, self.read_head);
-				if line.last() == Some(&b'\r') {
-					Some(&line[..line.len() - 1])
-				} else {
-					Some(line)
-				}
+			ReadLine => None,
+			ReadLineFinished => {
+				assert!(self.read_start <= self.read_end,
+						"read_start is bigger than read_end");
+				self.read_state = Ready;
+				self.read_head += 1;
+				Some(&self.internal_buffer
+					[self.read_start..self.read_end])
 			}
-			_ => panic!("invalid state")
+			_ => panic!("invalid operation; \
+			tried to read_line when state was {:?}", self.read_state)
+		}
+	}
+
+	pub fn read_until(&mut self, sequence: Vec<u8>) -> Option<&[u8]> {
+		match self.read_state {
+			Ready => {
+				self.read_start = self.read_head;
+				self.read_state = ReadUntil(sequence);
+				None
+			}
+			ReadUntil(_) => None,
+			ReadUntilFinished => {
+				assert!(self.read_start <= self.read_end,
+						"read_start is bigger than read_end");
+				self.read_state = Ready;
+				Some(&self.internal_buffer
+					[self.read_start..self.read_end])
+			}
+			_ => panic!("invalid operation; \
+			tried to read_until when state was {:?}", self.read_state)
+		}
+	}
+
+	pub fn read_exact(&mut self, size: usize) -> Option<&[u8]> {
+		match self.read_state {
+			Ready => {
+				self.read_start = self.read_head;
+				self.read_state = ReadExact(size);
+				None
+			}
+			ReadExact(_) => None,
+			ReadExactFinished => {
+				assert!(self.read_start <= self.read_end,
+						"read_start is bigger than read_end");
+				self.read_state = Ready;
+				Some(&self.internal_buffer
+					[self.read_start..self.read_end])
+			}
+			_ => panic!("invalid operation; \
+			tried to read_exact when state was {:?}", self.read_state)
 		}
 	}
 }
@@ -229,105 +196,55 @@ mod tests {
 		let mut buffer = BufferReader::default();
 		assert_eq!(buffer.read_state, Ready);
 		assert_eq!(buffer.advance(), true);
-		assert_eq!(buffer.read_token(), None);
+		assert_eq!(buffer.read_line(), None);
+		assert_eq!(buffer.read_state, ReadLine);
 		assert_eq!(buffer.advance(), false);
 	}
 
 	#[test]
-	fn simple_read_tokens() {
+	fn simple_read_lines() {
 		let mut buffer = BufferReader::default();
 		assert_eq!(buffer.read_state, Ready);
 
-		buffer.append("token1 token2 ".as_bytes());
-		assert_eq!(buffer.read_token(), None);
-		assert_eq!(buffer.read_state, ReadToken);
-		assert_eq!(buffer.advance(), true);
-		assert_eq!(buffer.read_state, Finished);
-		assert_eq!(buffer.read_token(), Some("token1".as_bytes()), "read_token != token1");
-		assert_eq!(buffer.read_state, Ready);
-
-		buffer.skip_spaces();
-		assert_eq!(buffer.read_state, SkipSpaces);
-		assert!(buffer.advance());
-		assert_eq!(buffer.read_state, Ready);
-
-		assert_eq!(buffer.read_token(), None);
-		assert_eq!(buffer.read_state, ReadToken);
-		assert_eq!(buffer.advance(), true);
-		assert_eq!(buffer.read_state, Finished);
-		assert_eq!(buffer.read_token(), Some("token2".as_bytes()));
-		assert_eq!(buffer.read_state, Ready);
-	}
-
-	#[test]
-	fn read_tokens_partial() {
-		let mut buffer = BufferReader::default();
-
-		buffer.append("tok".as_bytes());
-		assert_eq!(buffer.read_state, Ready);
-		assert_eq!(buffer.read_token(), None);
-		assert_eq!(buffer.read_state, ReadToken);
-		assert_eq!(buffer.advance(), false);
-		assert_eq!(buffer.read_state, ReadToken);
-
-		buffer.append("en ".as_bytes());
-		assert_eq!(buffer.read_token(), None);
-		assert_eq!(buffer.read_state, ReadToken);
-		assert_eq!(buffer.advance(), true);
-		assert_eq!(buffer.read_state, Finished);
-		assert_eq!(buffer.read_token(), Some("token".as_bytes()));
-		assert_eq!(buffer.read_state, Ready);
-	}
-
-	#[test]
-	fn simple_read_blob() {
-		let mut buffer = BufferReader::default();
-		assert_eq!(buffer.read_blob(10), None);
-		assert_eq!(buffer.read_state, ReadBlob(10));
-		buffer.append(&[1, 2, 3, 4]);
-
-		assert_eq!(buffer.advance(), false);
-		assert_eq!(buffer.read_blob(10), None);
-
-		buffer.append(&[5, 6, 7, 8, 9, 10]);
-		assert_eq!(buffer.advance(), true);
-		assert_eq!(buffer.read_blob(10),
-				   Some([1u8, 2, 3, 4, 5, 6, 7, 8, 9, 10].as_slice()));
-	}
-
-	#[test]
-	fn simple_read_line() {
-		let mut buffer = BufferReader::default();
-		buffer.append("line\r\nanother line\r\n".as_bytes());
+		buffer.append("line 1\r\nline 2\r\n".as_bytes());
 		assert_eq!(buffer.read_line(), None);
 		assert_eq!(buffer.read_state, ReadLine);
 		assert_eq!(buffer.advance(), true);
-		assert_eq!(buffer.read_state, Finished);
-		assert_eq!(buffer.read_line(), Some("line".as_bytes()));
+		assert_eq!(buffer.read_state, ReadLineFinished);
+		assert_eq!(buffer.read_line(), Some("line 1".as_bytes()),
+				   "reading first line failed");
+		assert_eq!(buffer.read_state, Ready);
+		assert_eq!(buffer.read_head, 8,
+				   "expecting head to be just after the first line");
+
+		// unnecessary sanity check
 		assert_eq!(buffer.advance(), true);
+		assert_eq!(buffer.read_state, Ready);
+		//
 
 		assert_eq!(buffer.read_line(), None);
 		assert_eq!(buffer.read_state, ReadLine);
 		assert_eq!(buffer.advance(), true);
-		assert_eq!(buffer.read_state, Finished);
-		assert_eq!(buffer.read_line(), Some("another line".as_bytes()));
+		assert_eq!(buffer.read_state, ReadLineFinished);
+		assert_eq!(buffer.read_line(), Some("line 2".as_bytes()),
+				   "reading second line failed");
+
+		assert_eq!(buffer.read_state, Ready);
 	}
 
 	#[test]
-	fn simple_read_line_2() {
+	fn simple_read_lines_simulate_async_buffer() {
 		let mut buffer = BufferReader::default();
 		buffer.append("  Lorem ipsum  \r\n".as_bytes());
 		assert_eq!(buffer.read_line(), None);
 		assert_eq!(buffer.read_state, ReadLine);
 		assert_eq!(buffer.advance(), true);
-		assert_eq!(buffer.read_state, Finished);
+		assert_eq!(buffer.read_state, ReadLineFinished);
 		assert_eq!(buffer.read_line(), Some("  Lorem ipsum  ".as_bytes()));
-		assert_eq!(buffer.read_state, SkipOne);
 
-		assert_eq!(buffer.read_head, 16);
-		assert_eq!(buffer.advance(), false);
-		assert_eq!(buffer.read_state, SkipOne);
-		assert_eq!(buffer.read_head, 16);
+		assert_eq!(buffer.read_head, 17,
+				   "head position after reading the 1st line should be after \\n, \
+				   at the end of the current buffer ie. at index 17");
 
 		buffer.append("  dolor sit amet".as_bytes());
 		assert_eq!(buffer.advance(), true);
@@ -345,8 +262,13 @@ mod tests {
 
 		buffer.append("\n".as_bytes());
 		assert_eq!(buffer.advance(), true);
-		assert_eq!(buffer.read_state, Finished);
-		assert_eq!(buffer.read_line(), Some("  dolor sit amet".as_bytes()));
+		assert_eq!(buffer.read_state, ReadLineFinished);
+		assert_eq!(buffer.read_line(), Some("  dolor sit amet".as_bytes()),
+				   "extraction of the second line, with only the CRLF trimmed out");
+
+		assert_eq!(buffer.read_head, 35,
+				   "head position after reading the 2nd line should be after \\n, \
+				   at index 35");
 
 		buffer.append("\n".as_bytes());
 		assert_eq!(buffer.advance(), true);
@@ -355,7 +277,9 @@ mod tests {
 		assert_eq!(buffer.read_line(), None);
 		assert_eq!(buffer.read_state, ReadLine);
 		assert_eq!(buffer.advance(), true);
-		assert_eq!(buffer.read_line(), Some("".as_bytes()));
+		assert_eq!(buffer.read_line(), Some("".as_bytes()),
+				   "pushed a CRLF immediately after the last read_line \
+				   and expecting an empty line");
 
 		buffer.append("\r\n".as_bytes());
 		assert_eq!(buffer.advance(), true);
@@ -364,18 +288,39 @@ mod tests {
 		assert_eq!(buffer.read_line(), None);
 		assert_eq!(buffer.read_state, ReadLine);
 		assert_eq!(buffer.advance(), true);
-		assert_eq!(buffer.read_line(), Some("".as_bytes()));
+		assert_eq!(buffer.read_line(), Some("".as_bytes()),
+				   "expecting an empty line again, sanity check");
+
+		assert_eq!(buffer.read_state, Ready);
 	}
 
 	#[test]
-	fn complex_read_simulation() {
+	fn read_until_on_simple_buffer() {
 		let mut buffer = BufferReader::default();
-
-		let appends = [
-			"GET".as_bytes(),
-			" ".as_bytes(),
-			" ".as_bytes(),
-		];
+		buffer.append("Lorem ipsum dolor sit amet= after boundary".as_bytes());
+		assert_eq!(buffer.read_until(Vec::from([b'='])), None);
+		assert_eq!(buffer.advance(), true);
+		assert_eq!(buffer.read_until(Vec::from([b'='])),
+				   Some(b"Lorem ipsum dolor sit amet".as_slice()),
+				   "trying to read all bytes until the '=' boundary");
+		assert_eq!(buffer.read_state, Ready);
 	}
 
+	#[test]
+	fn read_exact_simulate_async_buffer() {
+		let mut buffer = BufferReader::default();
+		assert_eq!(buffer.read_exact(10), None);
+		assert_eq!(buffer.read_state, ReadExact(10));
+		buffer.append(&[1, 2, 3, 4]);
+
+		assert_eq!(buffer.advance(), false);
+		assert_eq!(buffer.read_exact(10), None);
+
+		buffer.append(&[5, 6, 7, 8, 9, 10]);
+		assert_eq!(buffer.advance(), true);
+		assert_eq!(buffer.read_exact(10),
+				   Some([1u8, 2, 3, 4, 5, 6, 7, 8, 9, 10].as_slice()));
+
+		assert_eq!(buffer.read_state, Ready);
+	}
 }
