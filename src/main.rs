5d8640d8fd6fee998051e30990f9ad467d205bd8
saki <stanislaw.darowski101@gmail.com>
2026-05-12 21:51:42 +0200

    testing & doodling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,39 @@
 use http::message_common::{message_common_dbg, RequestCollector};
 
 fn main() {
-	let sample_normal = b"GET / HTTP/1.1\r\nHost: example.com\r\n\
-	Accept: */*\r\ncontent-length: 5\r\n\r\nhello world!";
+	test_request_collector(
+		"simple request",
+		b"GET / HTTP/1.1\r\n\
+		Host: example.com\r\n\
+		Accept: */*\r\n\
+		content-length: 5\r\n\
+		\r\n\
+		hello world!",
+	);
 
-	test_request_collector(sample_normal);
+	test_request_collector(
+		"multipart",
+		b"GET / HTTP/1.1\r\n\
+		Host: example.com\r\n\
+		content-length: 76\r\n\
+		content-type: multipart/form-data; boundary=ABCD\r\n\
+		\r\n\
+		--ABCD\r\n\
+		content-disposition: form-data; name=\"foo\"\r\n\
+		\r\n\
+		hello world!\r\n\
+		--ABCD--",
+	);
 }
 
+const C_MAG: &str = "\x1b[95m";
 const CYAN: &str = "\x1b[96m";
 const C_NUL: &str = "\x1b[0m";
 
 
-fn test_request_collector(sample: &[u8]) {
-	println!("{CYAN}==== Test ===={C_NUL}");
-	println!("request: ({} bytes)\n<<<<<{:.100}>>>>>", sample.len(),
+fn test_request_collector(test_name: &str, sample: &[u8]) {
+	println!("{C_MAG}===test: {test_name:?}{C_NUL}");
+	println!("request: ({} bytes)\n{CYAN}<<<<<{C_NUL}{:.100}{CYAN}>>>>>{C_NUL}", sample.len(),
 			 String::from_utf8_lossy(sample));
 	println!("{CYAN}--------------{C_NUL}");
 
@@ -32,4 +52,7 @@ fn test_request_collector(sample: &[u8]) {
 
 	println!("{CYAN}debug:{C_NUL}");
 	message_common_dbg(&rc);
+
+	println!("{C_MAG}---ended: {test_name:?}{C_NUL}");
+	print!("\n\n");
 }
--- src/proto/message_common.rs
+++ src/proto/message_common.rs
@@ -1,9 +1,9 @@
-use std::cmp::min;
 use crate::consts::{Method, StatusCode, Version};
 use crate::proto::message_common::CollectMessageState::Ended;
 use crate::proto::parser::ParseError::{InvalidHeader, RepeatedHeader};
 use crate::proto::parser::{parse_header_line, HeaderLineParseResult, ParseError};
 use crate::proto::state_buffer_reader::{BufferReader, StateBufferReaderResult};
+use std::cmp::min;
 use CollectMessageState::Processing;
 use ProcessingStage::{Body, FirstLine, PreludeHeaders};
 use StateBufferReaderResult::{Done, NotEnoughBytes};
@@ -38,6 +38,8 @@ where
 
 	content_length: Option<usize>,
 	multipart_boundary: Option<Vec<u8>>,
+
+	body_bytes: Vec<u8>,
 }
 
 trait MessageSpecific {
@@ -76,6 +78,8 @@ where
 
 			content_length: None,
 			multipart_boundary: None,
+
+			body_bytes: Vec::new(),
 		}
 	}
 
@@ -97,7 +101,7 @@ where
 				}
 				Processing(stage) => {
 					if !self.stage_match(stage) {
-						return
+						return;
 					}
 				}
 			}
@@ -134,7 +138,7 @@ where
 						match parse_header_line(header_line) {
 							HeaderLineParseResult::Empty => {
 								self.collect_state = Processing(Body);
-								return true;
+								true
 							}
 							HeaderLineParseResult::Ok {
 								field_name,
@@ -179,19 +183,34 @@ where
 										}
 									}
 								}
+
+								true
 							}
 							HeaderLineParseResult::Err(e) => {
 								self.collect_state = Ended(Err(e));
-								return false;
+								false
 							}
-						};
-
-						true
+						}
 					}
 				}
 			}
 			Body => {
-				return false;
+				match self.content_length {
+					None => {
+						self.collect_state = Ended(Ok(()));
+						false
+					}
+					Some(v) => {
+						match self.buffer1_reader.take_exact(&self.buffer1, v) {
+							NotEnoughBytes => false,
+							Done(s) => {
+								self.body_bytes.extend_from_slice(s);
+								self.collect_state = Ended(Ok(()));
+								false
+							}
+						}
+					}
+				}
 			}
 		}
 	}
@@ -266,4 +285,18 @@ where
 		println!("header: {:?}:{:?}",
 				 String::from_utf8_lossy(h.as_slice()), String::from_utf8_lossy(v.as_slice()));
 	}
+	println!("content-length: {:?}", msg.content_length);
+	println!("multipart boundary: {:?}", match &msg.multipart_boundary {
+		None => None,
+		Some(v) => Some(String::from_utf8_lossy(v.as_slice()))
+	});
+	println!();
+	println!("body: ({} bytes)", msg.body_bytes.len());
+	println!(
+		"<<<{:.100}>>>{}",
+		String::from_utf8_lossy(&msg.body_bytes),
+		if msg.body_bytes.len() > 100 {
+			format!("\n[+{} bytes]", msg.body_bytes.len() - 100)
+		} else { String::new() }
+	)
 }
